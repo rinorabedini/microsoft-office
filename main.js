7e675304ac591bf67abf44962642fa095ea9e2e5
Rinor Abedini <83778870+rinorabedini@users.noreply.github.com>
2021-07-30 18:05:44 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+const menuBtn = document.querySelector('.menu-btn');
+const mainMenu = document.querySelector('.main-menu');
+
+menuBtn.addEventListener('click', () => mainMenu.classList.toggle('show'));
